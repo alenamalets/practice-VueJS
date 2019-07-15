e7efef7620ba0c8caa38469eb11cb5a44e09af04
Alena <elen.malets@gmail.com>
2019-07-15 15:19:49 +0200

    listening to events click, mousemove

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ new Vue({
         },
     },
 })
+
+new Vue({
+    el: '#app2',
+    data: {
+        counter: 0,
+        x: 0,
+        y: 0,
+    },
+    methods: {
+        increase: function() {
+            this.counter++;
+        },
+        updateCoordinates: function(event) {
+            this.x = event.clientX;
+            this.y = event.clientY;
+        }
+    }   
+})
