46c4e517d4a6b35942f99db519a7967e919e9e82
Alena <elen.malets@gmail.com>
2019-07-15 15:35:37 +0200

    using event modifier

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ new Vue({
         updateCoordinates: function(event) {
             this.x = event.clientX;
             this.y = event.clientY;
-        }
+        },
+        // deadSpot:function (event) {
+        //     event.stopPropagation();
+            
+        // }
     }   
 })
