c798c93ad154c5bad9d978e6895e431f8ea2a785
Alena <elen.malets@gmail.com>
2019-07-15 13:35:01 +0200

    using v-bind, v-once directives

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World',
+        link: 'http://google.com',
     },
     methods: {
         changeTitle: function (event) {
             this.title = event.target.value;
-        }
+        },
+        sayHello: function (){
+            this.title = 'yeeeah'
+            return this.title  //access data using this keywords
+        },
     },
 })
