98e70a120396e7abaf32892d000d66b518a57b8a
Alena <elen.malets@gmail.com>
2019-07-17 13:41:07 +0200

    using for loop

--- part2.js
+++ part2.js
@@ -4,3 +4,14 @@ new Vue({
         show: true,
     },
 })
+
+new Vue({
+    el: '#app2',
+    data: {
+        ingredients: ["fruit", "bread", "fish"],
+        persons: [
+            {name: 'Bob', age: 27, color: 'blue'},
+            {name: 'Kate', age: 31, color: 'red'},
+        ]
+    },
+})
