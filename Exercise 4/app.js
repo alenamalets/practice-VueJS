6d11ace7ed1f2d6f90753eab2e3dfee3d6747163
Alena <elen.malets@gmail.com>
2019-07-17 10:00:57 +0200

    practice styling

--- Exercise 4/app.js
+++ Exercise 4/app.js
@@ -1,11 +1,37 @@
 new Vue({
   el: '#exercise',
   data: {
-
+      showEffect: {
+        highlight: false,
+        shrink: true,
+      },
+      changeClass: "",
+      visible: true,
+      myClass:{
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'orange'
+      },
+      prBar: {
+        width: '0px',
+        backgroundColor: 'orange',
+      }
   },
   methods: {
     startEffect: function() {
-    
+      const vm = this;
+      setInterval(() => {
+        vm.showEffect.highlight = !vm.showEffect.highlight,
+        vm.showEffect.shrink = !vm.showEffect.shrink  
+      }, 2000);
+    },
+    startProgressBar() {
+      const vm = this;
+      width = 0;
+      setInterval(() => {
+        width < 100 ? width = width + 10 : width=100;
+        vm.prBar.width = width + 'px';
+      }, 500);
     }
   }
 });
